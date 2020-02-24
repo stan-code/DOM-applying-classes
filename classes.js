357668efa530cc9bbde86ef5ca0b3c21fe05d724
Stan-Gogan <sjgogan@me.com>
2020-02-24 18:16:28 +1300

    manipulating dom dots

--- classes.js
+++ classes.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
+  two()
+  three() 
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -20,6 +23,22 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
-// CREATE FUNCTION three HERE
+function two () {
 
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+
+}
+
+// CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('tango')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+  
+}
